7b83c8ed46802ef4714fb2c4f07c392fdacb4694
Sanskar Sahu <sanskarsahu54@gmail.com>
2024-05-09 14:08:17 +0530

    modified:   app/(auth)/layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,13 @@
+import { LandingNavbar } from "@/components/landing-navbar";
+
 const AuthLayout = ({
     children
 }:
 {children:React.ReactNode;
 })=>{
     return(
-        <div className="flex items-center justify-center h-full">
+        <div className="flex items-center justify-center h-full bg-blue-950">
+            <LandingNavbar/>
             {children}
         </div>
     );
