ce53374d12680926c2d3032324a20ed979b36b35
Sanskar Sahu <sanskarsahu54@gmail.com>
2024-05-09 13:33:43 +0530

    modified:   components/landing-navbar.tsx

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -14,16 +14,16 @@ export const LandingNavbar = () => {
   const { isSignedIn } = useAuth();
 
   return (
-    <nav className="p-4 fixed top-0 w-full  backdrop-blur-xl flex items-center justify-between h-20">
+    <nav className="p-4 fixed top-0 w-[100vw]  backdrop-blur-xl flex items-center justify-between h-20">
       <Link href="/" className="flex items-center">
-        <div className="relative mr-1">
+        <div className="relative sm:mr-1">
           <Image width={90} height={90} alt="Logo" src="/logo.png" />
         </div>
-        <h1 className={cn("text-2xl font-bold text-white", font.className)}>
+        <h1 className={cn("sm:text-2xl text-lg font-bold text-white", font.className)}>
           HelpMate
         </h1>
       </Link>
-      <div className="flex items-center gap-x-2">
+      <div className="flex items-center sm:gap-x-2">
         <Link href={isSignedIn ? "/dashbord" : "/sign-up"}>
           <Button variant="outline" className="rounded-full bg-transparent border-s-gray-50 text-white">
             Get Started
