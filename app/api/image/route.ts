94b64a597e0980c85c9c34f6f314e5fb0c5c60d0
Sanskarsahu <85392227+Sanskarsahu@users.noreply.github.com>
2024-11-09 17:33:32 +0530

    Update route.ts

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -53,7 +53,7 @@ export async function POST(
         return new NextResponse("Free trial has expired.", { status: 403})
       }
   
-      const response = await replicate.run("stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4", { input });;
+      const response = await replicate.run("stability-ai/stable-diffusion-3.5-large", { input });;
 
       if (!isPro){
         await incrementApiLimit();
